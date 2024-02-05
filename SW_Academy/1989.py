579340f152241a5c56ee27e5f4b8b513002a96c7
부울경_1반_어지민 <eojm9388@gmail.com>
2024-02-05 16:02:18 +0900

    SW_academy 1989번 문제풀이

--- SW_Academy/1989.py
+++ SW_Academy/1989.py
@@ -1,13 +1,18 @@
+import sys
+sys.stdin = open('input.txt')
+
 T = int(input())
 
-for test_case in range(1, T + 1):
-    word = input()
-    word_list = list(word)
-    word_list.reverse()
-    word_reverse = ''.join(word_list)
+for tc in range(1, T+1):
+    # 입력받은 문자열
+    str1 = input()
+    # 뒤집은 문자열
+    str2 = str1[::-1]
 
-    if word == word_reverse:
-        print(f'#{test_case} 1')
+    print(f'#{tc}', end=' ')
+    # 입력받은 문자열이 뒤집은 문자열과 같다면 회문이다.
+    if str1 == str2:
+        print(1)
     else:
-        print(f'#{test_case} 0')
+        print(0)
 
