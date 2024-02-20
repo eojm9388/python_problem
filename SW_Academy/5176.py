bc126da139bc856e6c6538736cd2e143ed709083
부울경_1반_어지민 <eojm9388@gmail.com>
2024-02-20 15:28:05 +0900

    SW_academy 5176번 문제풀이

--- SW_Academy/5176.py
+++ SW_Academy/5176.py
@@ -10,9 +10,13 @@ def in_order(now):
     # now 값이 있다면
     # 중위순회
     if now != 0 and now <= N:
+        # 왼쪽 자식 노드
         in_order(now*2)
-        after_tree[now] = num
+        # 왼쪽 자식이 리턴되면
+        # 현재 탐색 번호 할당
+        tree[now] = num
         num += 1
+        # 오른쪽 자식 노드
         in_order(now*2+1)
 
 
@@ -23,10 +27,9 @@ for tc in range(1, T+1):
     # 노드에 들어갈 번호 -> 탐색 순서?
     num = 1
     # 완전 이진 트리
-    before_tree = [0] + list(range(1, N+1))
     # 중위순회를 거친 완전 이진 트리
-    after_tree = [0] * (N+1)
+    tree = [0] * (N+1)
     # 중위순회
     in_order(1)
     # 1번 노드가 루트가 된다.
-    print(f'#{tc}', after_tree[1], after_tree[(N//2)])
+    print(f'#{tc}', tree[1], tree[(N//2)])
