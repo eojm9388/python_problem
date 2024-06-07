cd69bb814fc7a31f79753c296f219e60f0be226a
부울경_1반_어지민 <eojm9388@gmail.com>
2024-06-07 13:34:04 +0900

    현대 연탄의 크기 문제풀이

--- 현대소프티어/연탄의 크기/연탄의크기.py
+++ 현대소프티어/연탄의 크기/연탄의크기.py
@@ -1,11 +1,11 @@
 N = int(input())
-
+# 집들의 난로 반지름
 arr = list(map(int, input().split()))
-
+# 범위가 전부 100이하였기때문에 완전탐색
 max_cnt = 0
-
 for i in range(2, 100):
     cnt = 0
+    # 연탄의 반지름의 배수인 난로의 개수 구하기
     for j in arr:
         if j % i == 0:
             cnt += 1
